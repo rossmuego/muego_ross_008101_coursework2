31ac2b29d71941be52d354f5208ede9ec11da54a
Ross Muego <rossmuego@gmail.com>
2018-03-19 18:14:36 +0000

    Initial commit

---
+++ server.js
@@ -0,0 +1,17 @@
+var express = require('express');
+var exphbs  = require('express-handlebars');
+
+var app = express();
+
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
+
+app.get('/', function (req, res) {
+    res.render('home');
+});
+
+app.get('/page1', function (req, res) {
+    res.render('new');
+});
+
+app.listen(3000);
